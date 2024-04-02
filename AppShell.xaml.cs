b87d8ef6ecce6958d480164d69198b88c3c3e700
Manuel Rompani <manuelrompani14@gmail.com>
2024-04-02 10:53:04 -0300

    Init

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Scanner
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Runtime.ExceptionServices;
+
+namespace Scanner
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+            barcodeReader.Options = new ZXing.Net.Maui.BarcodeReaderOptions
+            {
+               Formats = ZXing.Net.Maui.BarcodeFormat.QrCode,
+               AutoRotate = true,
+               Multiple = true
+            };
+        }
+
+        private void barcodeReader_BarcodesDetected(object sender, ZXing.Net.Maui.BarcodeDetectionEventArgs e)
+        {
+            var first = e.Results?.FirstOrDefault();
+            
+            if (first is null)
+                return;
+
+            Dispatcher.Dispatch(async () =>
+            {
+                await DisplayAlert("Detected", first.Value, "OK");
+            });
+        }
+    }
+
+}
